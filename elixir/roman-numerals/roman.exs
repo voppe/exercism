028f46c09e3f9dff27ec123b6d69ef027b4995ed
voppe <voppe@voppe.it>
2016-11-22 23:18:08 +0100

    Revisited elixir/roman-numerals

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -1,25 +1,33 @@
 defmodule Roman do
+  @numerals [
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"],
+  ]
+
   @doc """
   Convert the number to a roman number.
   """
   @spec numerals(pos_integer) :: String.t
   def numerals(number) do
-    factor(number)
+    @numerals
+    |> Enum.reduce({number, ""}, fn [digit, numeral], {number, result} ->
+      {quotient, remainder} = {div(number, digit), rem(number, digit)}
+
+      roman = result <> String.duplicate(numeral, quotient)
+
+      {remainder, roman} # I need to keep track of both remainder and result, unsure if there's a clearer way
+    end)
+    |> elem(1) # Get the result
   end
-  
-  # This is cheating
-  defp factor(number) when number >= 1000 do String.duplicate("M", div(number, 1000)) <> factor(rem(number, 1000)) end
-  defp factor(number) when number >= 900 do String.duplicate("CM", div(number, 900)) <> factor(rem(number, 900)) end
-  defp factor(number) when number >= 500 do String.duplicate("D", div(number, 500)) <> factor(rem(number, 500)) end
-  defp factor(number) when number >= 400 do String.duplicate("CD", div(number, 400)) <> factor(rem(number, 400)) end
-  defp factor(number) when number >= 100 do String.duplicate("C", div(number, 100)) <> factor(rem(number, 100)) end
-  defp factor(number) when number >= 90 do String.duplicate("XC", div(number, 90)) <> factor(rem(number, 90)) end
-  defp factor(number) when number >= 50 do String.duplicate("L", div(number, 50)) <> factor(rem(number, 50)) end
-  defp factor(number) when number >= 40 do String.duplicate("XL", div(number, 40)) <> factor(rem(number, 40)) end
-  defp factor(number) when number >= 10 do String.duplicate("X", div(number, 10)) <> factor(rem(number, 10)) end
-  defp factor(number) when number >= 9 do String.duplicate("IX", div(number, 9)) <> factor(rem(number, 9)) end
-  defp factor(number) when number >= 5 do String.duplicate("V", div(number, 5)) <> factor(rem(number, 5)) end
-  defp factor(number) when number >= 4 do String.duplicate("IV", div(number, 4)) <> factor(rem(number, 4)) end
-  defp factor(number) when number >= 1 do String.duplicate("I", div(number, 1)) <> factor(rem(number, 1)) end
-  defp factor(_) do "" end
 end
